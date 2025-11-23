e9ca2db80598d30ba1e89fa4e91d047c05276eda
lmcclean0510 <liammcclean0510@gmail.com>
2025-11-23 17:41:38 +0000

    Update rating-form.svelte

--- src/lib/components/books/rating-form.svelte
+++ src/lib/components/books/rating-form.svelte
@@ -81,7 +81,7 @@
 				<div class="flex justify-center">
 					<StarRating
 						value={rating}
-						onChange={(newRating) => rating = newRating}
+						onchange={(newRating) => rating = newRating}
 						size="lg"
 					/>
 				</div>
